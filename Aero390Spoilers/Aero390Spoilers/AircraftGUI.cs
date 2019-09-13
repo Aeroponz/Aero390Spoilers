4c870a6c8014dcf8a8b2c422ead645833bd55c52
Jean-Philippe Durand <jp.durand@hotmail.com>
2019-09-13 17:37:21 -0400

    Added CL-300 Wings. Added 3 Landing Gear state pictures.

--- Aero390Spoilers/Aero390Spoilers/AircraftGUI.cs
+++ Aero390Spoilers/Aero390Spoilers/AircraftGUI.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Aero390Spoilers
 {
-    public partial class Form1 : Form
+    public partial class AircraftGUI : Form
     {
-        public Form1()
+        public AircraftGUI()
         {
             InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- Aero390Spoilers/Aero390Spoilers/Program.cs
+++ Aero390Spoilers/Aero390Spoilers/Program.cs
@@ -16,7 +16,7 @@ namespace Aero390Spoilers
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AircraftGUI());
         }
     }
 }
