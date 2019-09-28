f81bcbdf931944db1d004d66e2184096f0fafb51
Jean-Philippe Durand <jp.durand@hotmail.com>
2019-09-28 12:11:38 -0400

    Enabling Data Pipes interprocess communication...

--- Aero390Spoilers/Aero390Spoilers/Program.cs
+++ Aero390Spoilers/Aero390Spoilers/Program.cs
@@ -4,6 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Aero390Spoilers;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using SharedMemory;
+using System.IO.Pipes;
+using System.IO;
+
+#if NET40Plus
+using System.Threading.Tasks;
+#endif
+
 
 namespace Aero390Spoilers
 {
@@ -15,9 +26,60 @@ namespace Aero390Spoilers
         [STAThread]
         static void Main()
         {
+
+            Mutex mutex = null;
+            Thread.Sleep(5000);
+
+            while (mutex == null)
+            {
+                try
+                {
+                    mutex = Mutex.OpenExisting("Global\\CSSimulation");
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Mutex not found on " + DateTime.Now.ToString());
+                    Thread.Sleep(3000);
+                }
+
+
+            }
+            Console.WriteLine("Application2 executed on " + DateTime.Now.ToString());
+
+            Console.WriteLine("Press <enter> to start client");
+            Console.ReadLine();
+
+            using (NamedPipeClientStream pipeClient =
+            new NamedPipeClientStream(".", "ToGUI", PipeDirection.In))
+            {
+
+                // Connect to the pipe or wait until the pipe is available.
+                Console.Write("Attempting to connect to pipe...");
+                pipeClient.Connect();
+
+                Console.WriteLine("Connected to pipe.");
+                Console.WriteLine("There are currently {0} pipe server instances open.",
+                   pipeClient.NumberOfServerInstances);
+                using (StreamReader sr = new StreamReader(pipeClient))
+                {
+                    // Display the read text to the console
+                    string temp;
+                    while ((temp = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine("Received from server: {0}", temp);
+                    }
+                }
+            }
+            Console.Write("Press Enter to continue...");
+            Console.ReadLine();
+
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new AircraftGUI());
+            Console.ReadLine();
         }
     }
 }
---
+++ Aero390Spoilers/CSSimulationNode/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+#if NET40Plus
+using System.Threading.Tasks;
+#endif
+
+namespace CSSimulationNode
+{
+    class Program
+    {
+        private static bool isNewMutexCreated = true;
+        private static Mutex mutex;
+
+        static void Main(string[] args)
+        {
+            mutex = new Mutex(true, "Global\\CSSimulation", out isNewMutexCreated);
+            //AppDomain.CurrentDomain.ProcessExit += new EventHandler(CurrentDomain_ProcessExit);
+            Console.WriteLine("Application1 executed on " + DateTime.Now.ToString());
+            Console.WriteLine("STARTING SIMULATION SERVER...");
+            //Console.ReadLine();
+
+            using (NamedPipeServerStream pipeServer =
+            new NamedPipeServerStream("ToGUI", PipeDirection.Out))
+            {
+                Console.WriteLine("ToGUI NamedPipeServerStream object created.");
+
+                // Wait for a client to connect
+                Console.Write("Waiting for client connection...");
+                pipeServer.WaitForConnection();
+
+                Console.WriteLine("Client connected.");
+                try
+                {
+                    // Read user input and send that to the client process.
+                    using (StreamWriter sw = new StreamWriter(pipeServer))
+                    {
+                        sw.AutoFlush = true;
+                        //Console.Write("Enter text: ");
+                        //sw.WriteLine(Console.ReadLine());
+                        sw.WriteLine("Connection Test... 1... 2...");
+                    }
+                }
+                // Catch the IOException that is raised if the pipe is broken
+                // or disconnected.
+                catch (IOException e)
+                {
+                    Console.WriteLine("ERROR: {0}", e.Message);
+                }
+
+            }
+
+            Console.WriteLine("End of Main(), time to autodestruct!");
+            Console.ReadLine();
+        }
+
+        static void CurrentDomain_ProcessExit(Object sender, EventArgs e)
+        {
+            if (isNewMutexCreated)
+            {
+                Console.WriteLine("Mutex Released");
+                mutex.ReleaseMutex();
+            }
+        }
+    }
+}
