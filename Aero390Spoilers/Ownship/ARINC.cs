20603a1b8682cdcc7dd121990a9fb052d0e96c6c
Jean-Philippe Durand <jp.durand@hotmail.com>
2019-10-06 13:40:00 -0400

    Get Sets for ARINC 429.

--- Aero390Spoilers/Ownship/ARINC.cs
+++ Aero390Spoilers/Ownship/ARINC.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Text;
 
 namespace Ownship
@@ -8,7 +8,7 @@ namespace Ownship
     {
         public ARINC()
         {
-            A429Message = 0b_0000_0000_0000_0000_0000_0000_0000_0000;
+            A429Message = new BitArray(32);
 
         }
 
@@ -20,11 +20,24 @@ namespace Ownship
 
         // BIT 32
         //Parity
-        public byte GetP()
+        public int GetP()
         {
-            return A429Message[0];
+            return Convert.ToInt32(A429Message.Get(0));
         }
-        public string SetP(string)
+        public void SetP()//Recheck Parity
+        {
+            int bitSum = 0;
+            for(int i = 1;i<32;i++)
+            {
+                if(A429Message[i])
+                {
+                    bitSum++;
+                }
+            }
+            if (bitSum % 2 == 0) A429Message[0] = false;
+            else A429Message[0] = true;
+        }
+
         //BITS 31 and 30 (2 Bits)
         //Sign/Status Matrix
         //| Bit 31 | Bit 30 | Meaning BCD                           | Meaning BNR           | Meaning Discrete |
@@ -32,22 +45,52 @@ namespace Ownship
         //|    0   |    1   | No Computed Data (NCD)                | NCD                   | NCD              |
         //|    1   |    0   | Functional Test (FT)                  | FT                    | FT               |
         //|    1   |    1   | Minus, South, West, Left, From, Below | Normal Operation (NO) | FW               |
-        string SSM { get; set; }
+        public BitArray GetSSM()
+        {
+            BitArray oSSM = new BitArray(2);
+            oSSM[0] = A429Message[1];
+            oSSM[1] = A429Message[2];
+            return oSSM;
+        }
+        public void SetSSM(BitArray iSSM)
+        {
+            A429Message[1] = iSSM[0];
+            A429Message[2] = iSSM[1];
+        }
+
         //BITS 29 to 11 (19 Bits)
         //BNR ONLY -- Bit 29 is the Sign Matrix -- Bit 28->11 are the data
         //| Bit 29 | Meaning BNR                           |
         //|    0   | Plus, North, East, Right, Above       |
         //|    1   | Minus, South, West, Left, From, Below |
         //ALL OTHER Bit 29 -> 11
-        string Data { get; set; }
+        public BitArray GetData()
+        {
+            BitArray oData = new BitArray(19);
+            for(int i = 3; i<22; i++)
+            {
+                oData[i - 3] = A429Message[i];
+            }
+            return oData;
+        }
+        public void SetData(BitArray iData)
+        {
+            for (int i = 3; i < 22; i++)
+            {
+                A429Message[i] = iData[i - 3];
+            }
+        }
+
         //BITS 10 and 9 (2 Bits)
         // Source-Destination Identifier (Transmiting system ID)
         string SDI { get; set; }
+
         //BITS 8 to 1 (8 Bits)
         //Label
         string Label { get; set; }
+
         //FULL ARINC MESSAGE (32 Bits)
-        uint A429Message { get; set; }
+        BitArray A429Message { get; set; }
 
         #endregion Parameters
     }
