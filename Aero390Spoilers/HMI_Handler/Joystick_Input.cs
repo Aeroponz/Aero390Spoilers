f3de892165a752ab4a7d665925c900da01753c8c
Tiago Strobino <tiagostrobino@hotmail.com>
2019-11-19 17:42:02 -0500

    JS button rework

--- Aero390Spoilers/HMI_Handler/Joystick_Input.cs
+++ Aero390Spoilers/HMI_Handler/Joystick_Input.cs
@@ -15,14 +15,11 @@ namespace Joystick_Input
         static Guid joystickGuid = new Guid("5f939150-07ab-11ea-8001-444553540000");
         Joystick joystick = new Joystick(directInput, joystickGuid);
         bool first_check = true;
-        int sound_delayer = 0;
+        int options_delay = 0;
 
-        SoundPlayer ding = new SoundPlayer("..\\..\\Resources\\Misc\\SeatBeltChime.wav");
-
-        //static void Main()
         public JS_Input()
         {
-           //Create buffer
+            //Create buffer
             joystick.Properties.BufferSize = 128;
 
             // Acquire the joystick
@@ -63,26 +60,18 @@ namespace Joystick_Input
             return joystick.GetCurrentState().Buttons[7];
         }
 
-        public void JS_Options_button()
+        public bool JS_Options_button()
         {
             joystick.Poll();
-            if (joystick.GetCurrentState().Buttons[11] && sound_delayer == 0) ding.Play();
+            if (joystick.GetCurrentState().Buttons[11] && options_delay == 0)
+            {
+                options_delay++;
+                return true;
+            }
 
-            sound_delayer++;
-            if (sound_delayer >= 5) sound_delayer = 0;
+            if (options_delay > 0) options_delay++;
+            if (options_delay >= 30) options_delay = 0;
+            return false;
         }
-
-        //static void Main()
-        //{
-        //    JS_Input test = new JS_Input();
-        //    while (true)
-        //    {
-        //        Console.Clear();
-        //        Console.WriteLine("X_axis: " + test.get_JS_X());
-        //        Console.WriteLine("Y_axis: " + test.get_JS_Y());
-        //        Console.WriteLine("Z_axis: " + test.get_JS_Throttle());
-        //        Thread.Sleep(50);
-        //    }
-        //}
     }
 }
