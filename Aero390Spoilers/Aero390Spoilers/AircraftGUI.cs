88f0e90ee1719766f60b22cfa809f14dbd4444a7
Tiago Strobino <tiagostrobino@hotmail.com>
2019-11-27 14:44:26 -0500

    Bug squatting

--- Aero390Spoilers/Aero390Spoilers/AircraftGUI.cs
+++ Aero390Spoilers/Aero390Spoilers/AircraftGUI.cs
@@ -805,9 +805,7 @@ namespace Aero390Spoilers
                 if (speed_alert >= 2) speed_alert = 0;
                 underspeed_warning = true;
             }
-            else underspeed_warning = false;
-            if (GUIOwnship.IasKts > 0 && SpoilerLever.Value < 0) GUIOwnship.IasKts -= (double)SpoilerLever.Value/-10;
-            
+            else underspeed_warning = false;          
             UnderspeedWarning();
             if (GUIOwnship.IasKts > 0 && SpoilerLever.Value < 0) GUIOwnship.IasKts -= (double)SpoilerLever.Value / -10;
 
